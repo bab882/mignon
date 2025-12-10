ea441345a4e2b743190d3aaa39678233445ac8e6
Bab Codes <chanitebrandon@gmail.com>
2025-12-10 10:19:42 +0100

    modification cadeaux

--- troll/src/pages/blog/Blog.jsx
+++ troll/src/pages/blog/Blog.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import gift from "../../assets/img/gift.png";
+
+import hero from "../../assets/img/book_hero.png";
+import gems from "../../assets/img/gems.png"
+import bat from "../../assets/img/book_bat.png";
+import shiny from "../../assets/img/Shiny_Ore.png";
+import glowy from "../../assets/img/Glowy_Ore.png";
+import star from "../../assets/img//Starry_Ore.png";
+
+import king from "../../assets/img/king.png";
+import queen from "../../assets/img/queen.png";
+import decor from "../../assets/img/background_noel.jpg"
 import styles from "./Blog.module.css";
 
 function Blog() {
@@ -10,44 +20,47 @@ function Blog() {
   return (
     <div className={styles.container}>
       <div>
-        <p className={styles.text}>Chef, les cadeaux sont prêts ! Lequel ouvrez-vous ? </p>
-        {/* <p className={styles.text}>Lequel ouvrez-vous ?</p> */}
+        <p className={styles.text}>Chef, les cadeaux sont prêts ! Lequel choisissez-vous ? </p>
         <div className={styles.grid}>
-        <button onClick={() => navigate("/gift1")} className={styles.btn}>
-          <img className={styles.gift} src={gift} alt="" />
+
+          <button onClick={() => navigate("/gift1")} className={styles.btn}>
+            <img className={styles.gift} src={hero} alt="" />
+          </button>
+
+          <button onClick={() => navigate("/gift2")} className={styles.btn}>
+            <img className={styles.giftGems} src={gems} alt="" />
+          </button>
+
+          <button onClick={() => navigate("/gift3")} className={styles.btn}>
+            <img className={styles.gift} src={bat} alt="" />
+          </button>
+
+          <button onClick={() => navigate("/gift4")} className={styles.btn}>
+            <img className={styles.gift} src={shiny} alt="" />
+          </button>
+
+          <button onClick={() => navigate("/gift5")} className={styles.btn}>
+            <img className={styles.gift} src={glowy} alt="" />
+          </button>
+
+          <button onClick={() => navigate("/gift6")} className={styles.btn}>
+            <img className={styles.gift} src={star} alt="" />
+          </button>
+
+          <button onClick={() => navigate("/gift7")} className={styles.btn}>
+            <img className={styles.giftHero} src={king} alt="" />
+          </button>
+
+          <button onClick={() => navigate("/gift8")} className={styles.btn}>
+            <img className={styles.giftHero} src={queen} alt="" />
+          </button>
+
+        <button onClick={() => navigate("/gift9")} className={styles.btn}>
+          <img className={styles.gift3} src={decor} alt="" />
         </button>
 
-        <button onClick={() => navigate("/gift2")} className={styles.btn}>
-          <img className={styles.gift} src={gift} alt="" />
-        </button>
-
-        <button onClick={() => navigate("/gift3")} className={styles.btn}>
-          <img className={styles.gift} src={gift} alt="" />
-        </button>
-
-        <button onClick={() => navigate("/gift4")} className={styles.btn}>
-          <img className={styles.gift} src={gift} alt="" />
-        </button>
-
-        <button onClick={() => navigate("/gift5")} className={styles.btn}>
-          <img className={styles.gift} src={gift} alt="" />
-        </button>
-
-        <button onClick={() => navigate("/gift6")} className={styles.btn}>
-          <img className={styles.gift} src={gift} alt="" />
-        </button>
-
-        <button onClick={() => navigate("/gift7")} className={styles.btn}>
-          <img className={styles.gift} src={gift} alt="" />
-        </button>
-
-        <button onClick={() => navigate("/gift8")} className={styles.btn}>
-          <img className={styles.gift} src={gift} alt="" />
-        </button>
       </div>
       </div>
-      
-      
     </div>
   );
 }
---
+++ troll/src/pages/gift9/Gift9.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Gift9() {
+  return (
+    <div>
+      toto
+    </div>
+  )
+}
+
+export default Gift9
